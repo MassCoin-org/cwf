9d0ae2956317f7b63f128156743a82ce39716797
j4ck <50369519+j4cegh@users.noreply.github.com>
2022-09-10 15:27:20 +0300

    chore: fix jest

--- src/cwf.test.ts
+++ src/cwf.test.ts
@@ -1,18 +1,16 @@
 import cwf, { Cwf } from './index';
 
+const app = cwf();
+
 describe('cwf', () => {
   it('should create', () => {
-    const app = cwf();
     expect(app).toBeInstanceOf(Cwf);
-    app.destroy();
   });
 
   it('should render views', async () => {
-    const app = cwf();
-    app.listen();
+    app.listen(4040);
 
-    const errPage = await fetch('http://127.0.0.1:3000/404');
+    const errPage = await fetch('http://127.0.0.1:4040/404');
     expect(await errPage.text()).toBe('404');
-    app.destroy();
   });
 });
